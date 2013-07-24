c739680563941d072d6c3b32f5e970f23aa025b3
李石羽 <ligary04@hotmail.com>
2013-07-24 15:36:54 +0800

    create project ios util

---
+++ iOS Util/iOS Util/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS Util
+//
+//  Created by Thierry on 13-7-24.
+//  Copyright (c) 2013年 Thierry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS Util/iOS Util/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS Util
+//
+//  Created by Thierry on 13-7-24.
+//  Copyright (c) 2013年 Thierry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
